e10497c17e0221876354fd86c6e512f27f03f35a
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2024-02-10 01:12:13 +0100

    I added data to the DB

--- EcommerceMVC/Data/ApplicationDbContext.cs
+++ EcommerceMVC/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace EcommerceMVC.Data
                 
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "Science", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
