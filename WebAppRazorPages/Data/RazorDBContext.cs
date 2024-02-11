057f425fe9fc23d930c7b72735f395513c3f4810
isoboye24 <75511801+isoboye24@users.noreply.github.com>
2024-02-12 00:28:28 +0100

    I setup EF Core, created DB and also category table

---
+++ WebAppRazorPages/Data/RazorDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WebAppRazorPages.Models;
+
+namespace WebAppRazorPages.Data
+{
+    public class RazorDBContext:DbContext
+    {
+        public RazorDBContext(DbContextOptions<RazorDBContext> options) : base(options)
+        {
+                
+        }        
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "Science", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- WebAppRazorPages/Program.cs
+++ WebAppRazorPages/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppRazorPages.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorDBContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
